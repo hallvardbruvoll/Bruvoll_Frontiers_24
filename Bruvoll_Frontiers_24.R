caac21a2c083739f2d5724288804d993ddad9b80
Hallvard Nikolai Bruvoll <hallvbru@uio.no>
2024-07-05 17:37:38 +0200

    First commit

--- Bruvoll_Frontiers_24.R
+++ Bruvoll_Frontiers_24.R
@@ -3,8 +3,38 @@ library(tidyverse)
 library(poweRlaw)
 
 # Data preparation --------------------------------------------------------
+  # Load data sets
+lbk_house_sizes <- read.csv2("Data/lbk_house_sizes.csv",
+                             encoding = "UTF-8") %>%
+  mutate_if(is.character, as.factor)
 
+tryp_house_sizes <- read.csv2("Data/trypillia_house_sizes.csv",
+                              encoding = "UTF-8") %>%
+  mutate_if(is.character, as.factor)
 
+# Tidy the data input so that the following code becomes redundant:
+
+## Make object with settlements and house sizes
+my_settlements <- bind_rows(filter(zitava_sites_only, region == "zitava_valley") %>%
+                              dplyr::select(house_size, Settlement, site_name_ill_short) %>%
+                              mutate(Culture = "Linear Pottery") %>%
+                              rename(site_name_ill = site_name_ill_short),
+                            tryp_house_sizes %>%
+                              dplyr::select(house_size, Settlement, site_name_ill, culture) %>%
+                              rename(Culture = culture))
+
+## Filter out settlements with n < 10 (too small for dist.fit); add Gini index
+my_settlements <- my_settlements %>%
+  group_by(Settlement) %>%
+  filter(n() > 10) %>%
+  mutate(Gini = round(Gini(house_size), 3))
+
+## Remove unused factor levels (should be 13 settlements left)
+my_settlements <- droplevels.data.frame(my_settlements)
+levels(my_settlements$Settlement)
+
+## Also, try to resolve the Vrable neighbourhood issue (remove the double
+## entry, rename column to just Neighbourhood)
 
 
 # Analysis ----------------------------------------------------------------
