b4fa9907682d9ec9b6e5e498b58775af741bafad
Hallvard Nikolai Bruvoll <hallvbru@uio.no>
2024-07-05 14:21:39 +0200

    First commit

---
+++ Bruvoll_Frontiers_24.R
@@ -0,0 +1,17 @@
+# Frontiers paper, script for analysis and plots
+library(tidyverse)
+library(poweRlaw)
+
+# Data preparation --------------------------------------------------------
+
+
+
+
+# Analysis ----------------------------------------------------------------
+
+
+
+
+# Figures -----------------------------------------------------------------
+
+
